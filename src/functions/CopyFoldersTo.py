dbb3ff6b582d534b95a5bb5310c5b15aaf7ef8fa
m0opha <m0opha>
2024-01-14 23:43:56 -0300

    modified:   src/functions/CopyFoldersTo.py 	modified:   src/main.py

--- src/functions/CopyFoldersTo.py
+++ src/functions/CopyFoldersTo.py
@@ -6,29 +6,33 @@ def CopyFoldersTo(folders:list, destination:str):
     
     copied_folder = {}
 
-    progress_logger = ProgressBarLogger()
-    progress_logger.initUI("[*] copying Folders to destination")    
+    #progress_logger = ProgressBarLogger()
+    #progress_logger.initUI("[*] copying Folders to destination")    
 
     incrementor = Incrementor()
     total_folder = len(folders)
 
     for _folder in folders:
+    
         try:
-            if (os.path.basename(_folder) in copied_folder) == False:
+            
+            if os.path.basename(_folder) not in copied_folder:
+                print(f"{_folder} : { os.path.join(destination, os.path.basename(_folder))}")
                 shutil.copytree(_folder, os.path.join(destination, os.path.basename(_folder)))
                 incrementor.increment()
                 copied_folder[os.path.basename(_folder)] = 0
 
             else:
                 copied_folder[os.path.basename(_folder)] += 1
-                shutil.copytree(_folder, os.path.join(destination, os.path.basename(_folder)+"_"+copied_folder[os.path.basename(_folder)]))
+                print(f"{_folder} : { os.path.basename(_folder)+"_"+str(copied_folder[os.path.basename(_folder)])}")
+                shutil.copytree(_folder, os.path.join(destination, os.path.basename(_folder)+"_"+str(copied_folder[os.path.basename(_folder)])))
                 incrementor.increment()                
 
-            progress_logger.drawProgressBar(int(incrementor.get()*100/total_folder))
-            progress_logger.log(f"Folder copied from {_folder} to {destination}")
+            #progress_logger.drawProgressBar(int(incrementor.get()*100/total_folder))
+            #progress_logger.log(f"Folder copied from {_folder} to {destination}")
 
         except Exception as e:
             print(f"Error copying the folder: {e}")
 
-        finally:        
-            progress_logger.close()
+         
+    #progress_logger.close()
--- src/main.py
+++ src/main.py
@@ -10,11 +10,12 @@ from .functions.modules import TraverseDirectoryTree
 
 def main():
     filename = "projectfinder.json"
-
     path , destine = ArgHandler()
     project_files_path = GetFiles(files_found=TraverseDirectoryTree(path), filename=filename)
     file_content = ReadFiles(project_files_path)
     folder_to_copy = ExtractPaths(content=file_content, key="path")
+    print(json.dumps(folder_to_copy, indent=2))
+
     CopyFoldersTo(folder_to_copy, destine)
     
     print(f"Total Project found : {len(folder_to_copy)}")
