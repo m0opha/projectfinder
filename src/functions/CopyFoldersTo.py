bfb5ff89bf4e52e42c149644b4a8332b2830d9ab
m0opha <m0opha>
2024-01-14 23:27:53 -0300

    modified:   src/functions/CopyFoldersTo.py

--- src/functions/CopyFoldersTo.py
+++ src/functions/CopyFoldersTo.py
@@ -4,6 +4,8 @@ from .modules import Incrementor, ProgressBarLogger
 
 def CopyFoldersTo(folders:list, destination:str):
     
+    copied_folder = {}
+
     progress_logger = ProgressBarLogger()
     progress_logger.initUI("[*] copying Folders to destination")    
 
@@ -12,17 +14,21 @@ def CopyFoldersTo(folders:list, destination:str):
 
     for _folder in folders:
         try:
-            shutil.copytree(_folder, os.path.join(destination, os.path.basename(_folder)))
-            incrementor.increment()
+            if (os.path.basename(_folder) in copied_folder) == False:
+                shutil.copytree(_folder, os.path.join(destination, os.path.basename(_folder)))
+                incrementor.increment()
+                copied_folder[os.path.basename(_folder)] = 0
+
+            else:
+                copied_folder[os.path.basename(_folder)] += 1
+                shutil.copytree(_folder, os.path.join(destination, os.path.basename(_folder)+"_"+copied_folder[os.path.basename(_folder)]))
+                incrementor.increment()                
 
             progress_logger.drawProgressBar(int(incrementor.get()*100/total_folder))
             progress_logger.log(f"Folder copied from {_folder} to {destination}")
 
         except Exception as e:
             print(f"Error copying the folder: {e}")
-        
-        finally:
+
+        finally:        
             progress_logger.close()
-
-    return incrementor.get()
-
